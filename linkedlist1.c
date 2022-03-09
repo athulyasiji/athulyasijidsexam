394357bd6a19b3bec243032d0b926d1fd968ff94
athulyasiji <94526349+athulyasiji@users.noreply.github.com>
2022-03-09 12:24:12 +0530

    Add files via upload

---
+++ linkedlist1.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+struct ptr*
+{
+struct node*
+{
+struct head*
+}
+{
+void push();
+void pop();
+}
+{
+struct ptr*=(struct node*)malloc(struct node);
+int item;
+if(ptr==NULL)
+{
+printf("over flow");
+}
+else
+{
+printf("enter the value");
+scanf("%d",&item);
+}
+if(head==NULL)
+{
+ptr->next=NULL;
+ptr->val=val;
+head=ptr;
+}
+else
+{
+ptr->next=head;
+ptr->val=val;
+head=ptr;
+}
+}
+void pop()
+{
+int data;
+struct ptr*
+if(head==NULL)
+{
+printf("underflow");
+}
+else
+{
+data=ptr->next;
+ptr->val;
+head->ptr;
+free(ptr);
+printf("item popped",$data);
+}
+}
+printf("operations");
+printf("/n----------------/n");
+printf("/n1.push/n2.pop");
+printf("Enter your choice");
+printf("%d"&choice);
+switch(n)
+(n!=0)
+case1:
+{
+push();
+break;
+}
+case2:
+{
+pop();
+break;
+}
+case3:
+{
+exit(0);
+break;
+}
+getch();
+}
+}
+};
